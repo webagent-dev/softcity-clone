921617d7e58681057a57d9a25610d7831b3bbe1d
webagent-dev <webagent24@gmail.com>
2022-05-04 02:22:03 +0100

    finish navbar component

--- src/comps/navbar/navbar.style.js
+++ src/comps/navbar/navbar.style.js
@@ -1,20 +1,65 @@
 import styled from 'styled-components'
-export const NavbarContainer = styled.div``
-export const Nav = styled.p``
-export const Wrapper = styled.div``
+export const NavbarContainer = styled.div`
+   width: 100%;
+   height: 120px;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+`
+export const Nav = styled.p`
+   cursor: pointer;
+position: relative;
+padding: 5px 0px;
+   &:after{
+         content: "";
+         width: 0;
+         height: 3px;
+         background: black;
+         position: absolute;
+         bottom: 0;
+         transition: 0.3s;
+   }
+  &:hover:after{
+     width: 100%;
+     left: 0;
+  }
+`
+export const Wrapper = styled.div`
+   width: 80%;
+   margin: 0  auto;
+   display: flex;
+   align-items: center;
+   gap: 2rem;
+     position: relative;
+`
  export const Logo = styled.img`
-    
+      height: 50px;
  `
  export const Options = styled.h3``
  export const OptionWrapper = styled.div``
- export const  NavWrapper = styled.div``
+ export const  NavWrapper = styled.div`
+      display: flex;
+      position: absolute;
+      align-items: center;
+      gap: 2rem;
+      bottom: 0;
+      right: 10%;
+      text-transform: uppercase;
+      font-weight: 700;
+      font-family: monospace;
+      font-size: 17px;
+ `
 
  export const Label = styled.h3`
- width: 350px;
+ width: auto;
     background: skyblue;
     padding: 5px 8px;
     font-family: monospace;
     text-transform: uppercase;
     color: white;
     text-align: center;
+    position: absolute;
+    right: 0;
+    top: 0;
+    margin-bottom: 10px;
  `
