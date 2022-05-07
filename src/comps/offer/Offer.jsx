f6bbb5d62c1814f92549bd7c2bb1bd8295a899ad
webagent-dev <webagent24@gmail.com>
2022-05-07 17:58:22 +0100

    complete offer componnt

--- src/comps/offer/Offer.jsx
+++ src/comps/offer/Offer.jsx
@@ -1,9 +1,125 @@
 import React from 'react'
-import { OfferContainer } from './offer.style'
+import { OfferContainer, Wrapper, LineContainer, Line, Header, Headers, UnderLine, UnderLineContainer, GridContainer, MainContainer,Content, TextContent, Mark, Small,Text,OtherContainer } from './offer.style'
+import { DiAndroid, DiApple  } from 'react-icons/di'
+import { BsUnlock,BsSunglasses } from 'react-icons/bs'
+import {  RiWindowLine, RiThumbUpLine,RiPieChart2Line  } from 'react-icons/ri'
+import {  GiCctvCamera,GiCelebrationFire } from 'react-icons/gi'
 function Offer() {
   return (
     <OfferContainer>
-      Offer Container 
+      <Wrapper>
+      <LineContainer>
+        <Line />
+        <Line />
+        <Line />
+      </LineContainer>
+      <Small>do more with Softcity</Small>
+       <Header>what we offer </Header>
+      <UnderLineContainer>
+        <OtherContainer>
+       <UnderLine />
+              <UnderLine />
+        </OtherContainer>
+        <OtherContainer>
+       <UnderLine />
+              <UnderLine />
+        </OtherContainer>
+        <OtherContainer>
+       <UnderLine />
+              <UnderLine />
+        </OtherContainer>
+      </UnderLineContainer>
+      <GridContainer>
+        <MainContainer>
+<DiAndroid   size='80px' />
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+            <Mark />
+          </Content>
+        </MainContainer>
+        <MainContainer>
+<DiAndroid  size='80px' />
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+            <Mark />
+          </Content>
+        </MainContainer>
+        <MainContainer>
+<DiAndroid  size='80px' />
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+
+          </Content>
+        </MainContainer>
+        <MainContainer>
+<DiAndroid  size='80px'  />
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+            <Mark />
+          </Content>
+        </MainContainer>
+        <MainContainer>
+<DiAndroid size='80px' />
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+            <Mark />
+          </Content>
+        </MainContainer>
+        <MainContainer>
+<DiAndroid  size='80px'  />
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+          </Content>
+        </MainContainer>
+        <MainContainer>
+<DiAndroid  size='80px' />
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+            <Mark />
+          </Content>
+        </MainContainer>
+        <MainContainer>
+<DiAndroid  size='80px' />
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+            <Mark />
+          </Content>
+        </MainContainer>
+        <MainContainer>
+<DiAndroid   size='80px'/>
+          <Content>
+            <TextContent>
+              <Headers>Android Development</Headers>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, neque rem vitae commodi ipsum beatae.</Text>
+          </TextContent>
+          </Content>
+        </MainContainer>
+      </GridContainer>
+      </Wrapper>
       </OfferContainer>
   )
 }
--- src/comps/offer/offer.style.js
+++ src/comps/offer/offer.style.js
@@ -1,3 +1,95 @@
 import styled from 'styled-components'
 
-export const OfferContainer = styled.div``
+export const OfferContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    margin-top: 30px;
+    padding: 20px;
+`
+
+export const Wrapper  =styled.div`
+    width: 80%;
+    margin: 0 auto;
+`
+export const  LineContainer =styled.div`
+width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .3rem;
+    margin: 10px 0px;
+`
+  export const Line =styled.div`
+    width: 15px;
+    height: 2px;
+    background: black;
+  `
+  export const  Header =styled.h1`
+    font-family: monospace;
+    color: red;
+    text-align: center;
+    text-transform: capitalize;
+    padding: 10px 0px;
+  `
+  export const Headers = styled.h1`
+       font-family: monospace;
+    color: red;
+    text-transform: capitalize;
+    padding: 10px 0px;
+  `
+  export const   UnderLine =styled.div`
+    width: 10px;
+    height: 1px;
+    background: black;
+
+  `
+    export const  UnderLineContainer =styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .1rem;
+        margin: 10px 0px;
+    `
+    export const OtherContainer = styled.div`
+        display: flex;
+        flex-direction: column;
+        gap: .1rem;
+    `
+     export const  GridContainer =styled.div`
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+        gap: 2.5rem;
+        margin-top: 30px;
+     `
+       export const MainContainer =styled.div`
+        display: flex;
+        gap: 2rem;
+       `
+      export const  Content =styled.div`
+        display: flex;
+        align-items: center;
+      `
+        export const TextContent =styled.div``
+        export const  Mark =styled.div`
+            width: 8px;
+            height: 80%;
+            background: lightgray;
+        `
+        export const Small = styled.h3`
+            text-align: center;
+            text-transform: capitalize;
+            font-weight: 500;
+            font-family: monospace;
+            padding: 10px 0px;
+        `
+        export const Text = styled.div`
+            color: gray;
+            font-family: monospace;
+            line-height: 20px;
+            font-size: 15px;
+            // justify-self: flex-end;
+            margin-left: 25px;
+
+        `
