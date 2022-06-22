b06e9d03c63b06d374ee92a4482e06ad1db2dba1
webagent-dev <webagent24@gmail.com>
2022-06-22 18:30:17 +0100

    fix slow video bug

--- src/comps/video/Video.jsx
+++ src/comps/video/Video.jsx
@@ -1,33 +1,32 @@
 import React, { useState, useEffect  } from 'react'
 import { VideoContainer, Container,Img, Vidoe, OverLay } from './video.style'
 function Video() {
-  const [ show, setShow] = useState(true)
-
-  const switchView = () => {
-    const playVideo = setTimeout(() => {
-        setShow(false)
-
-        return  () => clearTimeout(playVideo)
-    }, 2000)
-  }
+  const [show, setShow] = useState(true)
+  
   useEffect(() => {
+          const switchView = () => {
+      setTimeout(() => {
+        setShow(false)
+      }, 2000)
+    }
     switchView()
-  },[])
+  },[setShow])
+  
   return (
     <VideoContainer>
-       <Container>
-         {
-           show ?
-             <Img  src='/img/bg.jpg' alt='main-photo' />
-           : 
-           <Vidoe     autoPlay={true}  loop muted>
-             <source  
-             src='/img/home.mp4' 
-             type='video/mp4'
-             />
-           </Vidoe>
-         }
-        </Container>
+      <Container>
+        {
+          show ?
+            <Img src='/img/bg.jpg' alt='main-photo' />
+            :
+            <Vidoe autoPlay={true} loop muted>
+              <source
+                src='/img/home.mp4'
+                type='video/mp4'
+              />
+            </Vidoe>
+        }
+      </Container>
 
     </VideoContainer>
   )
